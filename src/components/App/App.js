1956e7e6d03c23440d4726e476e5f74cb2c166b0
Dustin Cook <dustin.cook@mines.sdsmt.edu>
2019-08-07 19:12:27 -0600

    all components will get their own sub dir
    a component may have its own styling or additional resources in its own dir in the components directory

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "../App.css";
-import Header from "./Header";
-import Footer from "./Footer";
+import "./App.css";
+import Header from "../Header";
+import Footer from "../Footer";
 
 function App() {
   return (
