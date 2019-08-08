4b3e9c972765a855e0165b36f9b7634b66e87c43
Dustin Cook <dustin.cook@mines.sdsmt.edu>
2019-08-07 19:24:31 -0600

    gave header n footer their own directories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
